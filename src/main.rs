40310abcc5fa8daf771334f700a5f57eb48a3b09
Guocork <g1024536444@gmail.com>
2024-12-25 22:10:09 +0800

    Register

--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,18 @@ fn main() {
 enum Instruction {
     
 }
+
+/// Resgisters are the memory units in the CPU that temporarily store data and instructions and address.
+enum Register {
+    R_R0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT,
+} 
